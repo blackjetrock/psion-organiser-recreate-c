c6b45edbade73ab850f5bb4cfcf7e13a1adb0f2e
blackrockjet <menadue@gmail.com>
2024-08-12 21:40:56 +0100

    Updates

---
+++ psion-recreate/oldbuilkd/CMakeFiles/3.20.0/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-/bin/sh: 1: /usr/bin/uname: not found")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "/bin/sh: 1: /usr/bin/uname: not found")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "unknown")
+
+include("/home/menadue/tree/downloaded_tools/pico/pico-sdk/cmake/preload/toolchains/pico_arm_gcc.cmake")
+
+set(CMAKE_SYSTEM "PICO")
+set(CMAKE_SYSTEM_NAME "PICO")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "cortex-m0plus")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
