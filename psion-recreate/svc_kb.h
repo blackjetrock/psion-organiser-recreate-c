996aae8c6eb72144b2e3ccb3593bca9f6cead2b0
blackrockjet <menadue@gmail.com>
2024-08-17 10:03:55 +0100

    Updates

--- psion-recreate/svc_kb.h
+++ psion-recreate/svc_kb.h
@@ -1,9 +1,9 @@
 typedef uint64_t MATRIX_MAP;
 
+
 void matrix_scan(void);
 
 #define NOS_KEY_BUFFER_LEN 16
-#define NOS_KEY_NONE -1
 
 extern volatile int nos_key_in;
 extern volatile int nos_key_out;
@@ -12,9 +12,12 @@ void nos_put_key(char key);
 char nos_get_key(void);
 void check_keys(void);
 
-#define KEY_ON     1
-#define KEY_UP     3
-#define KEY_DOWN   4
-#define KEY_LEFT   5
-#define KEY_RIGHT  6
-
+typedef enum _KEYCODE
+  {
+   KEY_ON = 1,
+   KEY_UP =   3,
+   KEY_DOWN = 4,
+   KEY_LEFT = 5,
+   KEY_RIGHT = 6,
+   NOS_KEY_NONE = -1,
+  } KEYCODE;
