d0694feaf24d7e6340dfd7974a10e768f8e61fd0
blackrockjet <menadue@gmail.com>
2024-08-22 10:19:37 +0100

    Updates

--- psion-recreate/switches.h
+++ psion-recreate/switches.h
@@ -2,16 +2,16 @@
 
 
 
-#define DB_GETK          1
+#define DB_GETK          0
 #define DB_DIGIT         0
-#define DB_PK_SAVE       1
-#define DB_PK_SETP       1
+#define DB_PK_SAVE       0
+#define DB_PK_SETP       0
 
 #define DB_FL_POS_AT_END 1
 #define DB_FL_SCAN_PAK   0
 #define DB_FL_CATL       0
-#define DB_FL_WRIT       1
+#define DB_FL_WRIT       0
 #define DB_FL_FREC       0
 #define DB_FL_READ       0
 #define DB_FL_SIZE       0
-#define DB_FL_FIND       1
+#define DB_FL_FIND       0
